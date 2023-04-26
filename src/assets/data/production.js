6a88faa765056121deff209ec64f7082eac10245
Polina <polina.lipse@gmai.com>
2023-04-26 17:10:11 +0300

    Initial commit

---
+++ src/assets/data/production.js
@@ -0,0 +1,242 @@
+import productImg01 from "../images/animal-habitat.webp";
+import productImg02 from "../images/animal-tower.webp";
+import productImg03 from "../images/bear-wound.webp";
+
+import productImg04 from "../images/birds-logic.webp";
+import productImg05 from "../images/candles.webp";
+import productImg06 from "../images/colors.webp";
+
+import productImg07 from "../images/healthy-habbits.webp";
+import productImg08 from "../images/juice.webp";
+import productImg09 from "../images/mushrooms.webp";
+import productImg10 from "../images/reading.webp";
+
+import productImg11 from "../images/shade-lotto-kitchen.webp";
+import productImg12 from "../images/shapes-sorter.webp";
+import productImg13 from "../images/snails-shapes.webp";
+import productImg14 from "../images/transport.webp";
+import productImg15 from "../images/words.webp";
+import productImg16 from "../images/ducks-bath.webp";
+
+import productImg17 from "../images/trash.webp";
+import productImg18 from "../images/muffins.webp";
+
+
+const products = [
+  {
+    id: "01",
+    productName: "Animal World",
+    imgUrl: productImg01,
+    category: "fiveYears",
+    price: 15.99,
+    shortDesc:
+      "Developmental kit set on spirals Animal World of 6 points of the world.",
+    description:
+      "The game Animal World is perfect for children who are already familiar with various types of animals and are starting to learn about their habitats on our planet. With the help of this game, the child learns and consolidates the names of animals from around the world, as well as learns the names of their habitats."
+  },
+
+  {
+    id: "02",
+    productName: "Animal Tower Building Set",
+    imgUrl: productImg02,
+    category: "fiveYears",
+    price: 12.99,
+    shortDesc:
+      "Developmental tool Animal Tower Building Set",
+    description:
+      "The game Animal Tower Building Set is perfect for children who are already familiar with the names of animals, as well as reinforcing their knowledge of object placement (bottom/top/over/under). With the help of this game, the child learns to work with schematics, reinforces animal names, learns the concepts of bottom/top, and trains memory."
+  },
+
+  {
+    id: "03",
+    productName: "Heal the Bear's Wound",
+    imgUrl: productImg03,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit Heal the Bear's Wound!",
+    description:
+      "The game Heal the Bears Wound is perfect for children who are starting to learn geometric shapes. ith the help of this game, the child learns and consolidates the names of geometric shapes, colors, names of body parts, and also the concepts of left and right."
+  },
+
+  {
+    id: "04",
+    productName: "Birds on the Fance",
+    imgUrl: productImg04,
+    category: "fiveYears",
+    price: 12.99,
+    shortDesc:
+      "Developmental kit Birds on the Fance",
+    description:
+      "The game Birds on the Fence is perfect for children who are already familiar with colors and are beginning to learn left and right. With the help of this game, the child learns to work with schemes, consolidates color names, learns the concepts of right/left, and also trains memory."
+  },
+
+  {
+    id: "05",
+    productName: "How Many Candles on the Cake?",
+    imgUrl: productImg05,
+    category: "threeYears",
+    price: 10.99,
+    shortDesc:
+      "Developmental kit How Many Candles on the Cake?",
+    description:
+      "The clip cards game How Many Candles on the Cake? is perfect for children who are just starting to learn to count from 1 to 5. Ask your child to count how many candles are on the cake, and then ask them to clip the peg onto the corresponding number. With the help of this game, the child learns to count, develops visual perception, and also trains fine motor skills."
+  },
+
+  {
+    id: "06",
+    productName: "Learning colors",
+    imgUrl: productImg06,
+    category: "oneYear",
+    price: 6.99,
+    shortDesc:
+      "Developmental kit Learning colors",
+    description:
+      "The game Learning colors is perfect for children who are just starting to learn colors, as well as beginning their acquaintance with animals. With the help of this game, the child will learn basic colors and also learn how to sort objects by colors."
+  },
+
+  {
+    id: "07",
+    productName: "Healthy Habits",
+    imgUrl: productImg07,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit Healthy Habits",
+    description:
+      "The game Healthy and Unhealthy is perfect for children who are interested in learning about what affects the health of our teeth. With the help of this game, the child forms an understanding of healthy and unhealthy foods, that healthy teeth depend on the food we eat, and also develops a careful attitude towards their own health."
+  },
+
+  {
+    id: "08",
+    productName: "What Juice is in the Glass?",
+    imgUrl: productImg08,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit What Juice is in the Glass?",
+    description:
+      "The game What Juice is in the Glass? is perfect for children to develop their speech skills; forming adjectives from nouns. For example: apple - apple-flavored. With the help of this game, the child develops speech and learns to form color associations."
+  },
+
+  {
+    id: "09",
+    productName: "Collect Mushrooms in the Basket",
+    imgUrl: productImg09,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit Collect Mushrooms in the Basket",
+    description:
+      "The reusable handwriting worksheets Collect Mushrooms in the Basket are perfect for children who are starting to prepare their hand for writing. With the help of these worksheets, the child will learn to correctly position their hand when writing, coordinate hand movements with their gaze, and also develop concentration."
+  },
+
+  {
+    id: "10",
+    productName: "Making Words",
+    imgUrl: productImg10,
+    category: "fiveYears",
+    price: 10.99,
+    shortDesc:
+      "Developmental kits Making Words",
+    description:
+      "The game Making Words is perfect for children who are already familiar with letters and are starting to learn to read. With the help of this game, the child learns to make words from 3 syllables, learns to read, and also expands their vocabulary."
+  },
+
+  {
+    id: "11",
+    productName: "Shadow Lotto: Kitchen",
+    imgUrl: productImg11,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit Shadow Lotto: Kitchen",
+    description:
+      "The game Kitchen is perfect for children who are beginning to learn about kitchen items. With this game, the child learns to form an image by silhouette, learns to find similarities and differences, and also learns to notice, observe, and remember details.",
+  },
+
+  {
+    id: "12",
+    productName: "Shape Sorter",
+    imgUrl: productImg12,
+    category: "fiveYears",
+    price: 10.99,
+    shortDesc:
+      "Developmental kit Shape Sorter",
+    description:
+      "The game Shape Sorter is perfect for children who are already familiar with geometric shapes. With the help of this game, the child learns to recognize the shapes of everyday objects, as well as form associations."
+  },
+  {
+    id: "13",
+    productName: "Help the Snail Find its Home",
+    imgUrl: productImg13,
+    category: "oneYear",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit Help the Snail Find its Home",
+    description:
+      "The game Help the Snail Find its Home is perfect for children who are learning geometric shapes. With the help of this game, the child learns and consolidates the names of geometric shapes and colors."
+  },
+
+  {
+    id: "14",
+    productName: "What Matches the Transport?",
+    imgUrl: productImg14,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit What Matches the Transport? ",
+    description:
+      "The game What Matches the Transport? is perfect for children who are starting their introduction to vehicles and the places they travel. With the help of this game, the child learns and consolidates the names of vehicles (airplane, ship), learns the names of modes of transportation (flies, sails), and also learns to classify the places where they travel (sky, sea)."
+  },
+
+  {
+    id: "15",
+    productName: "Making words",
+    imgUrl: productImg15,
+    category: "fiveYears",
+    price: 10.99,
+    shortDesc:
+      "Developmental kit Making Words",
+    description:
+      "The Making words game is perfect for children who are already familiar with letters and are beginning to learn to read. With the help of this game, the child learns to form words of three syllables, learns to read, and expands their vocabulary."
+  },
+
+  {
+    id: "16",
+    productName: "What is the sum?",
+    imgUrl: productImg16,
+    category: "fiveYears",
+    price: 10.99,
+    shortDesc:
+      "The developmental kit What is the sum?",
+    description:
+      "The card game What is the sum? is perfect for children who already have a good understanding of counting from 1 to 10, and are beginning to learn addition. Ask your child to add the numbers together, and then ask them to attach a clip to the corresponding number. With this game, children learn to add numbers, develop visual perception, and also train their fine motor skills."
+  },
+
+  {
+    id: "17",
+    productName: "Sorting the waste",
+    imgUrl: productImg17,
+    category: "fiveYears",
+    price: 12.99,
+    shortDesc:
+      "Developmental kit Sorting the waste",
+    description:
+      "The Sorting Waste game is perfect for children who are starting to learn about environmental pollution. With this game, it will be easier for the child to understand the importance of sorting waste, learn about the criteria by which waste is sorted, and show that each of us can make our world cleaner and better."
+  },
+
+  {
+    id: "18",
+    productName: "How many chocolates are in the cupcake?",
+    imgUrl: productImg18,
+    category: "threeYears",
+    price: 8.99,
+    shortDesc:
+      "Developmental kit How many chocolates are in the cupcake?",
+    description:
+      "The clip cards How many chocolates in the cupcake? game is perfect for children who are learning to count from 1 to 10. Ask your child to count how many chocolates are in the cupcake and then ask them to clip the corresponding number with a wooden peg. Through this game, children learn to count, develop visual perception, and practice fine motor skills."
+  },
+
+]
+export default products;
