c3f99f367f7f885ae5967189260e1ff18422649c
Polina <polina.lipse@gmai.com>
2023-04-30 21:26:33 +0300

    added media queries for navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import React, {useRef, useEffect } from 'react';
 import "./Header.css";
 import { Container, Row } from "reactstrap";
 import logo from "../../assets/images/smart-logo.svg";
@@ -22,12 +23,37 @@ const nav__links = [
 ];
 
 const Header = () => {
+
+  const headerRef = useRef(null);
+
+  const menuRef = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (
+        document.body.scrollTop > 80 ||
+        document.documentElement.scrollTop > 80
+      ) {
+        headerRef.current.classList.add("sticky__header");
+      } else {
+        headerRef.current.classList.remove("sticky__header");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  const menuToggle = () => menuRef.current.classList.toggle('active__menu');
+
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav__wrapper">
-            <div className="navigation">
+            <div className="navigation" ref={menuRef} onClick={menuToggle}>
               <ul className="menu">
                 {nav__links.map((item, index) => (
                   <li className="nav__item" key={index}>
@@ -58,10 +84,11 @@ const Header = () => {
                 <span className="badge">1</span>
               </div>
               <FaUserAlt className="user__icon" size={22} />
+              <div className="mobile__menu">
+                <span onClick={menuToggle}>
+                <GiHamburgerMenu size={22} />
+                </span>
             </div>
-
-            <div className="mobile__menu">
-              <GiHamburgerMenu size={22} />
             </div>
           </div>
         </Row>
