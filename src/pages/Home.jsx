e3d34127250b0dc0ca45f66fdfc995734a890556
Polina <polina.lipse@gmai.com>
2023-05-11 11:32:17 +0300

    added nativation to shop now btn

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -40,7 +40,7 @@ const Home = () => {
               speech, logical thinking, creative imagination and much more ⭐
             </p>
             <motion.button whileTap={{ scale: 1.2 }} className="buy__btn">
-              <Link to="shop">SHOP NOW</Link>
+              <Link to="/shop">SHOP NOW</Link>
             </motion.button>
           </div>
         </Container>
